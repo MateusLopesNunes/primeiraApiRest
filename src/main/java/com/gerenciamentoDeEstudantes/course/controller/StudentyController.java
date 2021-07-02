6192219bd48a1abd9f18cb2dcc6ff4c1ce769b30
MateusLopesNunes <mateuslopesnunes@outlook.com>
2021-07-01 22:10:02 -0300

    Project created

---
+++ src/main/java/com/gerenciamentoDeEstudantes/course/controller/StudentyController.java
@@ -0,0 +1,15 @@
+package com.gerenciamentoDeEstudantes.course.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentyController {
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/gerenciamentoDeEstudantes/course/PrimeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentoDeEstudantes.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
